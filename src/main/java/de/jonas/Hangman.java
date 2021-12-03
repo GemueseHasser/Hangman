bda1bac1df4020e5ee900b348d4ef01c11b50a02
Jonas Lobe <jonas.lobe.2006@outlook.com>
2021-12-03 14:49:11 +0100

    Initial commit

---
+++ src/main/java/de/jonas/Hangman.java
@@ -0,0 +1,12 @@
+package de.jonas;
+
+import org.jetbrains.annotations.NotNull;
+
+@NotNull
+public final class Hangman {
+
+    public static void main(@NotNull final String[] args) {
+
+    }
+
+}
